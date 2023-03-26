4ddcdf3299b370d284d4cc336ccfb02d35c50035
Lara Sena <laraf.sena@gmail.com>
2023-03-26 14:56:19 -0300

    Exercícios de classes

---
+++ Aluno - Cálculo de média e aprovação/Aluno - Cálculo de média e aprovação/Aluno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aluno___Cálculo_de_média_e_aprovação
+{
+    internal class Aluno
+    {
+        public string nome;
+        public double notaQuiz;
+        public double notaProva;
+        public double notaTrabalho;
+
+        public double CalcularMedia()
+        {
+            string nome = this.nome;
+            double media = (this.notaQuiz + this.notaProva + this.notaTrabalho) / 3;
+            string mediaFormatada = media.ToString("N2");
+            double.Parse(mediaFormatada);
+
+            if (media > 6)
+                Console.WriteLine("Parabéns {0}! Sua média foi de {1}, você foi aprovado(a)!", nome, mediaFormatada);
+
+            else if (media < 4)
+                Console.WriteLine("Que pena {0}, sua média foi de {1}. Você foi reprovado(a)!", nome, mediaFormatada);
+
+            else
+                Console.WriteLine("{0}, sua média foi de {1}. Você está de recuparação", nome, mediaFormatada);
+
+            return media;
+        }
+    }
+}
---
+++ Aluno - Cálculo de média e aprovação/Aluno - Cálculo de média e aprovação/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aluno___Cálculo_de_média_e_aprovação
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a1 = new Aluno();
+            a1.nome = "Thalia";
+            a1.notaQuiz = 10.0;
+            a1.notaProva = 6.4;
+            a1.notaTrabalho = 7.2;
+
+            Aluno a2 = new Aluno();
+            a2.nome = "Luiz";
+            a2.notaQuiz = 6.0;
+            a2.notaProva = 4.8;
+            a2.notaTrabalho = 5.1;
+
+            Aluno a3 = new Aluno();
+            a3.nome = "Romeu";
+            a3.notaQuiz = 3.6;
+            a3.notaProva = 4.1;
+            a3.notaTrabalho = 3.9;
+
+            a1.CalcularMedia();
+            a2.CalcularMedia();
+            a3.CalcularMedia();
+            Console.ReadLine();
+        }
+    }
+}
