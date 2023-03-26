e089517f20135e43780dc83ddecc0fa1be104c43
Lara Sena <laraf.sena@gmail.com>
2023-03-26 14:12:32 -0300

    Exercícios de classes

---
+++ Funcionario - Cálculo de aumento salarial/Funcionario - Cálculo de aumento salarial/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionario___Cálculo_de_aumento_salarial
+{
+    internal class Funcionario
+    {
+            public string nome;
+            public float salario;
+
+            public float CalcularSalario()
+            {
+                Console.WriteLine("Qual a porcentagem do aumento do salário?");
+                float valor = float.Parse(Console.ReadLine());
+
+                float porcentagem = (valor / 100) + 1;
+                this.salario = this.salario * porcentagem;
+
+                Console.WriteLine("O novo salário é de " + salario);
+                Console.ReadLine();
+
+                return this.salario;
+            }
+    }
+}
---
+++ Funcionario - Cálculo de aumento salarial/Funcionario - Cálculo de aumento salarial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionario___Cálculo_de_aumento_salarial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            f1.nome = "Lara";
+            f1.salario = 2500.00f;
+
+            f1.CalcularSalario();
+        }
+    }
+}
